9b4fa60bb9135623330678e7ce123d93ee20d75d
Henrique Luiz <heenluy@gmail.com>
2023-01-02 18:17:25 -0300

    build: initial configuration

---
+++ src/main/java/dev/henriqueluiz/peoplemanager/PeopleManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.henriqueluiz.peoplemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagerApplication.class, args);
+	}
+
+}
