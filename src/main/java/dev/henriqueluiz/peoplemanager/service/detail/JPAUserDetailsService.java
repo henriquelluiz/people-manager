ab80f906ef390d6f3b5ec6114792b3b896ff0251
Henrique Luiz <heenluy@gmail.com>
2023-01-03 14:16:00 -0300

    chore(auth): implements spring user details

---
+++ src/main/java/dev/henriqueluiz/peoplemanager/service/detail/JPAUserDetailsService.java
@@ -0,0 +1,26 @@
+package dev.henriqueluiz.peoplemanager.service.detail;
+/*
+ * @Author: Henrique Luiz
+ * @LinkedIn: heenluy
+ * @Github: heenluy
+ */
+
+import dev.henriqueluiz.peoplemanager.model.detail.JPAUserDetails;
+import dev.henriqueluiz.peoplemanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JPAUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(JPAUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User not found: '%s'", email)));
+    }
+}
