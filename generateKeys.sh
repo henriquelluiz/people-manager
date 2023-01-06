8ec6df69f15838f3d448baa4a0acbb5b319d7a97
Henrique Luiz <heenluy@gmail.com>
2023-01-06 14:24:22 -0300

    refactor(shell): updates generateKeys file

--- generateKeys.sh
+++ generateKeys.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CERTS_DIR=src/main/resources/certs
+CERTS_DIR_TEST=src/test/resources
 
 echo "[DEPLOY] Gerando chaves RSA com openssl"
 
@@ -10,6 +11,7 @@ exit
 fi
 
 [ ! -d  $CERTS_DIR ] && { mkdir $CERTS_DIR; }
+[ ! -d  $CERTS_DIR_TEST ] && { mkdir $CERTS_DIR_TEST; }
 
 echo "[DEPLOY] Criando o par de chaves"
 openssl genrsa -out $CERTS_DIR/keypair.pem 2048
@@ -19,5 +21,7 @@ openssl rsa -in $CERTS_DIR/keypair.pem -pubout -out $CERTS_DIR/public.pem
 
 echo "[DEPLOY] Extraindo chave privada"
 openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in $CERTS_DIR/keypair.pem -out $CERTS_DIR/private.pem
+
+cp -r $CERTS_DIR $CERTS_DIR_TEST
 
 echo "[DEPLOY] Tudo pronto!"
